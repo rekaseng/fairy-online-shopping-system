c7d584a4aec5cde2d216edb6d1806511ed29e92e
Rekaseng <55165601+rekaseng@users.noreply.github.com>
2022-08-02 09:23:38 +0800

    Add files via upload

--- src/database/MyDatabase.java
+++ src/database/MyDatabase.java
@@ -1,1 +1,35 @@
+package database;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class MyDatabase {
+	static Connection conn=null;
+	public static Connection doConnection() throws ClassNotFoundException, SQLException
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/fairyonlineshopping","root","");
+		return conn;
+	}
+	
+
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(MyDatabase.doConnection());
+		} catch (ClassNotFoundException | SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+	
+	
